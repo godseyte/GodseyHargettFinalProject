774a6c1323b1c3afe57079b5451ad69496bf370e
Alexander Hargett <hargetaj@mail.uc.edu>
2019-04-17 19:48:54 -0400

    Updated the product class with properties.

--- GroceryStoreSimulator/Product.cs
+++ GroceryStoreSimulator/Product.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Alex Hargett
+ * Final Assignment
+ * Due 5/1/2019
+ * Web Server Application Development IT3047 001
+ * 
+ * Models a product
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +16,23 @@ namespace GroceryStoreSimulator
 {
     class Product
     {
+        // Properties
+        private string mProductName; //The name of the product
+        private int mQuantity; //The amount of said item
+
+        //Getters and Setters
+        public string productName
+        {
+            get { return mProductName; }
+            set { mProductName = value; }
+        }
+
+        public int quantity
+        {
+            get { return mQuantity; }
+            set { mQuantity = value; }
+        }
+
+        //Constructor
     }
 }
