6bd3b16e9a506aa3bcd46edb7dabdc14599301b2
godseyte <godseyte@mail.uc.edu>
2019-04-17 19:41:55 -0400

    Moved Login, Added Store

---
+++ GroceryStoreSimulator/Store.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Tucker Godsey
+ * Final Assignment
+ * Due 5/1/2019
+ * Web Server Application Development IT3047 001
+ * 
+ * Models a Store
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryStoreSimulator
+{
+    class Store
+    {
+    }
+}
