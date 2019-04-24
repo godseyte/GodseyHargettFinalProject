6ccd803e4e94c0ec5a17e7af83789c5b59044bd0
Tucker Godsey <tucker.godsey@gmail.com>
2019-04-24 12:13:37 -0400

    Started adding functionality to Default's cs

--- GroceryStoreSimulator/OnlineOrderingWebPage/Default.aspx.cs
+++ GroceryStoreSimulator/OnlineOrderingWebPage/Default.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
@@ -15,6 +16,9 @@ public partial class _Default : System.Web.UI.Page
     // User Login
     protected void btnLoginUser_Click(object sender, EventArgs e)
     {
+        // Gather the input data and use it to create an Order server object.
 
+        // Create new Order object
+        //Order newOrder = new Order();
     }
 }
