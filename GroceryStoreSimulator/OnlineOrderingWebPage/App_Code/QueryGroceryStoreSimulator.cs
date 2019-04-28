63d24a7c529e124054d07e5deb6d18c6d9457ffd
Tucker Godsey <tucker.godsey@gmail.com>
2019-04-28 18:53:16 -0400

    Created QueryGroceryStoreSimulator for all classes to use

---
+++ GroceryStoreSimulator/OnlineOrderingWebPage/App_Code/QueryGroceryStoreSimulator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for QueryGroceryStoreSimulator
+/// </summary>
+public class QueryGroceryStoreSimulator
+{
+    private DataTable QueryGroceryStoreSimulatorDataTable(string query)
+    {
+        // New SQL Connection
+        SqlConnection cnn;
+
+        // New Connection String
+        string connetionString = ConfigurationManager.ConnectionStrings["GroceryStoreSimulatorConnectionString"].ConnectionString;
+
+        // Instantiate the SQL Connection with Connection String
+        cnn = new SqlConnection(connetionString);
+
+        // Create a SqlCommand with the input Query
+        SqlCommand command = new SqlCommand(query, cnn);
+
+        cnn.Open();
+
+        // Create a SqlDataReader and tie it to the query
+        //SqlDataReader reader = command.ExecuteReader();
+
+        // Create a DataTable to store all data and a SqlDataAdapter to fill the DataTable
+        DataTable dataTable = new DataTable();
+
+        // Fill the DataTable
+        //da.Fill(dataTable);
+        dataTable.Load(command.ExecuteReader());
+
+        cnn.Close();
+
+        return dataTable;
+    }
+}
--- GroceryStoreSimulator/OnlineOrderingWebPage/Default.aspx.cs
+++ GroceryStoreSimulator/OnlineOrderingWebPage/Default.aspx.cs
@@ -25,37 +25,8 @@ public partial class _Default : System.Web.UI.Page
         
     }
 
-    // Query GroceryStoreSimulator and return the reader
-    private DataTable QueryGroceryStoreSimulator (string query)
-    {
-        // New SQL Connection
-        SqlConnection cnn;
-
-        // New Connection String
-        string connetionString = ConfigurationManager.ConnectionStrings["GroceryStoreSimulatorConnectionString"].ConnectionString;
-
-        // Instantiate the SQL Connection with Connection String
-        cnn = new SqlConnection(connetionString);
-
-        // Create a SqlCommand with the input Query
-        SqlCommand command = new SqlCommand(query, cnn);
-
-        cnn.Open();
-
-        // Create a SqlDataReader and tie it to the query
-        //SqlDataReader reader = command.ExecuteReader();
-
-        // Create a DataTable to store all data and a SqlDataAdapter to fill the DataTable
-        DataTable dataTable = new DataTable();
-
-        // Fill the DataTable
-        //da.Fill(dataTable);
-        dataTable.Load(command.ExecuteReader());
-
-        cnn.Close();
-
-        return dataTable;
-    }
+    
+    
 
     // User Login
     protected void btnLoginUser_Click(object sender, EventArgs e)
